ba1bfcde42987876774e2ce7a71fc235de051ba8
hat-ctr <82428578+hat-ctr@users.noreply.github.com>
2021-04-16 20:52:42 -0400

    Hi! All

---
+++ AbeProject/src/just/comabe/abeclass.java
@@ -0,0 +1,11 @@
+package just.comabe;
+
+public class abeclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello everyone");
+	}
+
+}
